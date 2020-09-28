fcd1f8706fdf735168f95ef006f070a20153b689
Yuri Halushko <36885543+snaipp1@users.noreply.github.com>
2020-09-28 16:15:10 +0300

    feat: add max, min, and avg functions

--- src/index.js
+++ src/index.js
@@ -1,12 +1,4 @@
 
-exports.min = function min (array) {
-  return 0;
-}
-
-exports.max = function max (array) {
-  return 0;
-}
-
-exports.avg = function avg (array) {
-  return 0;
-}
+exports.min = array => (!array || array.length === 0) ?  0 : Math.min.apply(null, array);
+exports.max = array => (!array || array.length === 0) ? 0 : Math.max.apply(null, array);
+exports.avg = array => (!array || array.length === 0) ? 0 : array.reduce((sum, item) => (sum + item), 0) / array.length;
